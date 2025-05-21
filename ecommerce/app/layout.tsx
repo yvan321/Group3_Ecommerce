048ae69ac0669d8f22e5013e78015349455bf2ac
yvan321 <neilyvancaliwan@gmail.com>
2025-05-22 05:42:31 +0800

    Fixing globals.css

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
 import { ThemeProvider } from 'next-themes';
+import '@/assets/styles/globals.css';
 
 
 const inter = Inter({ subsets: ['latin'] });
