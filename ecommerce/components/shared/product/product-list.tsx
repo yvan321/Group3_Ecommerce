1b256424d40dd8be47146084c63652ed6a243258
yvan321 <neilyvancaliwan@gmail.com>
2025-05-22 04:14:03 +0800

    Initial Deploy

--- ecommerce/components/shared/product/product-list.tsx
+++ ecommerce/components/shared/product/product-list.tsx
@@ -1,8 +1,6 @@
 import ProductCard from './product-card';
 import { Product } from '@/types';
 
-
-
 const ProductList = ({
   data,
   title,
@@ -12,19 +10,16 @@ const ProductList = ({
   title?: string;
   limit?: number;
 }) => {
-  // Apply limit if provided, otherwise show all products
   const limitedData = limit ? data.slice(0, limit) : data;
 
   return (
     <div className='my-10'>
       <h2 className='h2-bold mb-4'>{title}</h2>
-      {limitedData.length > 0 ? (
+      {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {
-  data.map((product: Product) => (
-    <ProductCard key={product.slug} product={product} />
-  ))
-}
+          {limitedData.map((product: Product) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
         </div>
       ) : (
         <div>
