d83dc190aea513d98129c52b44ee2e6b51e34410
yvan321 <neilyvancaliwan@gmail.com>
2025-05-22 16:39:09 +0800

    Fixing styles 12

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '@/assets/styles/globals.css';
+import './globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from '@/components/ui/sonner';
