fb62c543cc9f74a916f099e8dd94bf972a7777e6
yvan321 <neilyvancaliwan@gmail.com>
2025-05-22 19:02:29 +0800

    FIxing styles18

---
+++ ecommerce/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
