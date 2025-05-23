c7a940b18b800dd361bb30ffbd83a6ae0664a679
yvan321 <neilyvancaliwan@gmail.com>
2025-05-24 04:52:37 +0800

    Callback url redirect2

--- ecommerce/auth.ts
+++ ecommerce/auth.ts
@@ -1,10 +1,11 @@
-import { compareSync } from 'bcrypt-ts-edge';
-import type { NextAuthConfig } from 'next-auth';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-
-import { prisma } from '@/db/prisma';
+import { authConfig } from './auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db/prisma';
+import { cookies } from 'next/headers';
+import { compare } from './lib/encrypt';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const config = {
   pages: {
@@ -12,16 +13,14 @@ export const config = {
     error: '/sign-in',
   },
   session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60,
+    strategy: 'jwt' as const,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       credentials: {
-        email: {
-          type: 'email',
-        },
+        email: { type: 'email' },
         password: { type: 'password' },
       },
       async authorize(credentials) {
@@ -33,13 +32,15 @@ export const config = {
             email: credentials.email as string,
           },
         });
-        // Check if user exists and password is correct
+
+        // Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = compareSync(
+          const isMatch = await compare(
             credentials.password as string,
             user.password
           );
-          // If password is correct, return user object
+
+          // If password is correct, return user
           if (isMatch) {
             return {
               id: user.id,
@@ -49,22 +50,76 @@ export const config = {
             };
           }
         }
-        // If user doesn't exist or password is incorrect, return null
+        // If user does not exist or password does not match return null
         return null;
       },
     }),
   ],
   callbacks: {
-    async session ({ session, user, trigger, token }: any) {
-      // Set the user id on the session
+    ...authConfig.callbacks,
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
       session.user.id = token.sub;
-      // If there is an update, set the name on the session
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
+
       return session;
     },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
+      }
+
+      // Handle session updates
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
+
+      return token;
+    },
   },
-} satisfies NextAuthConfig;
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
