4f70619720410dbd8c7955039c8d1d3bb6275599
yvan321 <neilyvancaliwan@gmail.com>
2025-06-06 04:44:43 +0800

    Added PayPal Payments2

--- ecommerce/lib/actions/order.actions.ts
+++ ecommerce/lib/actions/order.actions.ts
@@ -10,7 +10,6 @@ import { prisma } from '@/db/prisma';
 import { CartItem, PaymentResult, ShippingAddress } from '@/types';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
-import { PAGE_SIZE } from '../constants';
 import { Prisma } from '@prisma/client';
 
 
