4a073d5f2bacb81a2312f22e76ac1d106a2e0d81
yvan321 <neilyvancaliwan@gmail.com>
2025-06-18 03:31:56 +0800

    Testing Function server2

--- ecommerce/app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ ecommerce/app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -17,7 +17,7 @@ const SuccessPage = async (props: {
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  // Retrieve payment intent
+  // Retrieve payment intents
   const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
 
   // Check if payment intent is valid
